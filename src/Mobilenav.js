0044230c296cf8f2a348130ddebf4f2f6a619299
mahilday <mathildaimadojiemu@gmail.com>
2020-06-12 08:08:05 +0100

    added a mobile nav dropdown

--- src/Mobilenav.js
+++ src/Mobilenav.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import Menu from './assets/menu.svg'
-import Menubad from './assets/menubad.svg'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Menu from "./assets/menu.svg";
+import Menubad from "./assets/menubad.svg";
+import { Link } from "react-router-dom";
 import {
   HomeRounded,
   ExploreRounded,
@@ -11,61 +11,74 @@ import {
 } from "@material-ui/icons";
 
 class Mobilenav extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            isOpen: false
-        }
-    }
-    render() {
-        return (
-          <div>
-            {this.state.isOpen ? (
-              <img
-                className="dropdown menu"
-                onClick={() => alert("clicked")}
-                src={Menubad}
-                alt="menu"
-              />
-            ) : (
-              <img
-                className="dropdown menu"
-                onClick={() => alert("clicked")}
-                src={Menu}
-                alt="menu"
-              />
-            )}
-            <div className="bg-light mobile-nav">
-              <ul className="list-unstyled">
-                <li>
-                  <Link to="/" className="text-dark">
-                    <HomeRounded />
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/" className="text-dark">
-                    <ExploreRounded />
-                    Explore
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/" className="text-dark">
-                    <PersonAddRounded />
-                    SignUp
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/" className="text-dark">
-                    <AccountCircleRounded />
-                    SignIn
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  handleMenuClick = () => {
+    this.setState({ isOpen: true });
+  };
+  handleMenubadClick = () => {
+    this.setState({ isOpen: false });
+  };
+  render() {
+    return (
+      <div className="mobilewrapper">
+        {this.state.isOpen ? (
+          <img
+            className="dropdown menubad"
+            onClick={this.handleMenubadClick}
+            src={Menubad}
+            alt="menu"
+          />
+        ) : (
+          <img
+            className="dropdown menu"
+            onClick={this.handleMenuClick}
+            src={Menu}
+            alt="menu"
+          />
+        )}
+        <div
+          className={
+            this.state.isOpen
+              ? "mobile-nav"
+              : "d-none mobile-nav "
+          }
+        >
+          <ul className="bg-light list-unstyled">
+            <li>
+              <Link to="/" className="text-dark">
+                <HomeRounded />
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="text-dark">
+                <ExploreRounded />
+                Explore
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="text-dark">
+                <PersonAddRounded />
+                SignUp
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="text-dark">
+                <AccountCircleRounded />
+                SignIn
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Mobilenav;
